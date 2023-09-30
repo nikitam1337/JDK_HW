79ba47e23d8c5bd13dd0395ac8560bb04c428db1
nikitam1337 <nikitam.l@mail.ru>
2023-09-30 23:05:00 +0300

    Add serverWondow

--- HW_1/ProjectServer/src/main/java/ru/geekbrains/ServerWindow.java
+++ HW_1/ProjectServer/src/main/java/ru/geekbrains/ServerWindow.java
@@ -1,4 +1,50 @@
 package ru.geekbrains;
 
-public class ServerWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerWindow extends JFrame {
+    private static final int POS_X = 500;
+    private static final int POS_Y = 500;
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+
+    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStop = new JButton("Stop");
+    private final JTextArea log = new JTextArea();
+    private boolean isServerWorking;
+
+    ServerWindow(){
+        isServerWorking = false;
+        btnStop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = false;
+                System.out.println("Server stopped " + isServerWorking + "\n");
+            }
+        });
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = true;
+                System.out.println("Server started " + isServerWorking + "\n");
+            }
+        });
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat server");
+        setAlwaysOnTop(true);
+        JPanel panBottom = new JPanel(new GridLayout(1,2));
+        panBottom.add(btnStart);
+        panBottom.add(btnStop);
+        add(panBottom, BorderLayout.SOUTH);
+        add(log);
+
+
+        setVisible(true);
+    }
 }
