567e15a4089b0779f79b799814098b1e5f4d7eb2
nikitam1337 <nikitam.l@mail.ru>
2023-10-01 18:30:49 +0300

    Add client GUI

--- HW_1/ProjectServer/src/main/java/ru/geekbrains/ClientGUI.java
+++ HW_1/ProjectServer/src/main/java/ru/geekbrains/ClientGUI.java
@@ -1,4 +1,40 @@
 package ru.geekbrains;
 
-public class ClientGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientGUI extends JFrame {
+    private static final int POS_X = 700;
+    private static final int POS_Y = 500;
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+    private final JPanel panelTop = new JPanel(new GridLayout(2,3));
+    private final JButton btnLogin = new JButton("Login");
+    private final JButton btnSend = new JButton("Send");
+    private final JTextField fieldLogin = new JTextField("Login");
+    private final JTextField fieldPassword = new JTextField("Password");
+    private final JTextField fieldIP = new JTextField("IP Adders");
+    private final JTextField fieldPort= new JTextField("Port");
+    private final JTextField fieldMessage= new JTextField();
+    private final JTextArea areaMessages = new JTextArea();
+
+
+
+    ClientGUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat Client");
+        setAlwaysOnTop(true);
+
+        panelTop.add(fieldIP);
+        panelTop.add(fieldPort);
+        panelTop.add(fieldLogin);
+        panelTop.add(fieldPassword);
+        panelTop.add(btnLogin);
+        add(panelTop, BorderLayout.NORTH);
+
+        setVisible(true);
+    }
+
 }
